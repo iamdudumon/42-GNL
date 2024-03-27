8be1fb95e1a76f48a3c42f1e2caa361b5d31b3d7
iamdudumon <kksy4511@naver.com>
2024-03-28 01:55:14 +0900

    refactor: strlcat을 삭제하고 각 헬퍼함수에서 복사도록 변경 + feat: lstnew 메소드 추가 -> 새로운 노드를 생성하는 함수

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -33,41 +33,25 @@ char	*ft_get_chridx(const char *s, int c)
 	return (0);
 }
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
-{
-	size_t	dst_len;
-	size_t	src_len;
-	size_t	i;
-
-	if (!dst && size == 0)
-		return (0);
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	if (dst_len > size)
-		return (src_len + size);
-	i = 0;
-	while (*src != '\0' && dst_len + i + 1 < size)
-	{
-		dst[dst_len + i] = *src++;
-		i++;
-	}
-	dst[dst_len + i] = 0;
-	return (dst_len + src_len);
-}
-
 char	*ft_strdup(const char *s)
 {
 	char	*dup;
+	size_t	s_len;
 	size_t	len;
 
 	if (!s)
 		return (ft_strdup(""));
-	len = ft_strlen(s);
-	dup = (char *)malloc(sizeof(char) * (len + 1));
+	s_len = ft_strlen(s);
+	dup = (char *)malloc(sizeof(char) * (s_len + 1));
 	if (!dup)
 		return (0);
-	dup[0] = '\0';
-	ft_strlcat(dup, s, len + 1);
+	len = 0;
+	while (len < s_len)
+	{
+		dup[len] = s[len];
+		len++;
+	}
+	dup[len] = '\0';
 	return (dup);
 }
 
@@ -76,18 +60,46 @@ char	*ft_strjoin(char *s1, char *s2)
 	char	*join_str;
 	size_t	s1_len;
 	size_t	s2_len;
+	size_t	len;
 
-	if (!s1 || !s2)
-		return (0);
+	// if (!s1 || !s2)
+	// 	return (0);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	join_str = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!join_str)
 		return (0);
-	join_str[0] = '\0';
-	ft_strlcat(join_str, s1, s1_len + 1);
-	ft_strlcat(join_str, s2, s1_len + s2_len + 1);
+	len = 0;
+	while (len < s1_len)
+	{
+		join_str[len] = s1[len];
+		len++;
+	}
+	while (len < s1_len + s2_len)
+	{
+		join_str[len] = s2[len - s1_len];
+		len++;
+	}
+	join_str[len] = '\0';
 	free(s1);
 	free(s2);
 	return (join_str);
+}
+
+t_list	*lstnew(int fd, char *backup)
+{
+	t_list	*node;
+
+	if (!backup)
+		return (0);
+	node = (t_list *)malloc(sizeof(t_list) * 1);
+	if (!node)
+	{
+		free(backup);
+		return (0);
+	}
+	node->fd = fd;
+	node->backup = backup;
+	node->next = 0;
+	return (node);
 }

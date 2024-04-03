ed262d3177a360abc0ffa077507adb1da2e06b64
iamdudumon <kksy4511@naver.com>
2024-04-03 22:44:47 +0900

    fix: 더 이상 읽을 file 내용이 없고 backup이 empty일 때 null을 반환해 종료하도록 수정

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -12,6 +12,24 @@
 
 #include "get_next_line_bonus.h"
 
+char	*read_buff_size(int fd)
+{
+	ssize_t	size;
+	char	*buf;
+
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (0);
+	size = read(fd, buf, BUFFER_SIZE);
+	if (size == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[size] = '\0';
+	return (buf);
+}
+
 void	delete_backup_node(t_list **backup_list, t_list *target)
 {
 	t_list	*ptr;
@@ -54,41 +72,26 @@ t_list	*find_backup_node(t_list *backup_list, int fd)
 	t_list	*ptr;
 	char	*buf;
 
+	buf = read_buff_size(fd);
+	if (!buf)
+		return (0);
 	ptr = backup_list;
 	while (ptr->fd != fd && ptr->next != 0)
 		ptr = ptr->next;
 	if (ptr->fd == fd)
-	{
-		
+	{	
+		ptr->backup = ft_strjoin(ptr->backup, buf);
+		if (!(ptr->backup))
+			return (0);
 		return (ptr);
 	}
-	buf = (char *)malloc(sizeof(char) * 2);
-	if (!buf || read(fd, buf, 1) <= 0)
+	if (*buf == '\0')
 	{
 		free(buf);
 		return (0);
 	}
-	buf[1] = '\0';
 	ptr->next = lstnew(fd, buf);
 	return (ptr->next);
-}
-
-char	*read_buff_size(int fd)
-{
-	ssize_t	size;
-	char	*buf;
-
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buf)
-		return (0);
-	size = read(fd, buf, BUFFER_SIZE);
-	if (size == -1)
-	{
-		free(buf);
-		return (0);
-	}
-	buf[size] = '\0';
-	return (buf);
 }
 
 char	*merge_line(int fd, char **backup)
@@ -124,61 +127,82 @@ char	*get_next_line(int fd)
 	static t_list	*backup_list;
 	t_list			*node;
 	char			*newline;
+	char			*merge;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
 	if (!backup_list)
 	{
-		backup_list = lstnew(fd, ft_strdup(""));
+		merge = read_buff_size(fd);
+		if (merge && *merge == '\0')
+		{
+			free(merge);
+			return (0);
+		}
+		backup_list = lstnew(fd, merge);
 		if (!backup_list)
 			return (0);
 	}
 	node = find_backup_node(backup_list, fd);
 	if (!node)
 		return (0);
-	newline = ft_get_sub_newline(merge_line(node->fd, &(node->backup)), *(node->backup) == '\0');
-	if (!newline)
-	{
+	merge = merge_line(node->fd, &(node->backup));
+	newline = ft_get_sub_newline(merge, !(node->backup));
+	if (!newline || !(node->backup))
 		delete_backup_node(&backup_list, node);
-		return (0);
-	}
 	return (newline);
 }
 
-#include <stdio.h>
-#include <fcntl.h>
+// #include <stdio.h>
+// #include <fcntl.h>
 
-int main(){
-	int fd1 = open("./test1.txt", O_RDONLY);
-	int fd2 = open("./test2.txt", O_RDONLY);
-	int fd3 = open("./test3.txt", O_RDONLY);
-	int i = 0;
+// int main(){
+// 	int fd1 = open("./42_with_nl", O_RDONLY);
+// 	int fd2 = open("./42_with_nl", O_RDONLY);
+// 	int fd3 = open("./42_with_nl", O_RDONLY);
+// 	int i = 0;
 
-	while (1){
-		char *line1= get_next_line(fd1);
-		char *line2 = get_next_line(fd2);
-		char *line3 = get_next_line(fd3);
+// 	while (1){
+// 		char *line1= get_next_line(fd1);
+// 		char *line2 = get_next_line(fd2);
+// 		char *line3 = get_next_line(fd3);
 
-		if (line1)
-			printf("line1->%d: %s", i, line1);
-		else
-			printf("line1->%d: null\n", i);
-		if (line2)
-			printf("line2->%d: %s", i, line2);
-		else
-			printf("line2->%d: null\n", i);
-		if (line3)
-			printf("line3->%d: %s", i, line3);
-		else
-			printf("line3->%d: null\n", i);
-		free(line1);
-		free(line2);
-		free(line3);
-		i++;
-		if (!line1 && !line2 && !line3)// 
-			break;
-	}
-	close(fd1);
-	close(fd2);
-	close(fd3);
-}
+// 		if (line1)
+// 			printf("line1->%d: %s", i, line1);
+// 		else
+// 			printf("line1->%d: null\n", i);
+// 		if (line2)
+// 			printf("line2->%d: %s", i, line2);
+// 		else
+// 			printf("line2->%d: null\n", i);
+// 		if (line3)
+// 			printf("line3->%d: %s", i, line3);
+// 		else
+// 			printf("line3->%d: null\n", i);
+// 		free(line1);
+// 		free(line2);
+// 		free(line3);
+// 		i++;
+// 		if (!line1 && !line2 && !line3)//  
+// 			break;
+// 	}
+// 	close(fd1);
+// 	close(fd2);
+// 	close(fd3);
+// }
+
+// int main(){
+// 	int fd1 = open("./42_with_nl", O_RDONLY);
+// 	int i = 0;
+
+// 	while (1){
+// 		char *line1= get_next_line(fd1);
+
+// 		if (!line1)
+// 			break;
+// 		printf("line1->%d: %s", i, line1);
+// 		free(line1);
+// 		i++;
+// 	}
+// 	close(fd1);
+// }

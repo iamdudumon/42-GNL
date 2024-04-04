64b5df03c24a2e6aa154216b839270efc54b536b
iamdudumon <kksy4511@naver.com>
2024-04-04 16:16:58 +0900

    fix: header file ifndef 변경

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 19:56:30 by dukim             #+#    #+#             */
-/*   Updated: 2024/03/15 20:08:51 by dukim            ###   ########.fr       */
+/*   Updated: 2024/04/04 16:16:28 by dukim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <unistd.h>

2f5b4ebed34088ac7ff33e5bf4bbb639862aad26
iamdudumon <kksy4511@naver.com>
2024-04-05 19:18:09 +0900

    fix: backup 변수에 대입한 뒤 사용하지 않고 바로 backup_list를 참조하도록 변경 + main 문 삭제

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -78,64 +78,23 @@ char	*merge_line(int fd, char **backup)
 char	*get_next_line(int fd)
 {
 	static char	*backup_list[4096];
-	char		*backup;
 	char		*merge;
 	char		*newline;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-	backup = backup_list[fd];
-	if (!backup)
+	if (!backup_list[fd])
 	{
-		backup = ft_strdup("");
-		if (!backup)
+		backup_list[fd] = ft_strdup("");
+		if (!backup_list[fd])
 			return (0);
 	}
-	backup = is_readable_fd(fd, backup);
-	if (!backup)
+	backup_list[fd] = is_readable_fd(fd, backup_list[fd]);
+	if (!backup_list[fd])
 		return (0);
-	merge = merge_line(fd, &backup);
-	newline = ft_get_sub_newline(merge, !backup);
-	backup_list[fd] = backup;
+	merge = merge_line(fd, &backup_list[fd]);
+	newline = ft_get_sub_newline(merge, !backup_list[fd]);
 	if (!newline)
-		free(backup);
+		free(backup_list[fd]);
 	return (newline);
-}
-
-#include <stdio.h>
-#include <fcntl.h>
-
-int main(){
-	int fd1 = open("files/test1.txt", O_RDONLY);
-	int fd2 = open("files/test2.txt", O_RDONLY);
-	int fd3 = open("files/test3.txt", O_RDONLY);
-	int i = 0;
-
-	while (1){
-		char *line1= get_next_line(fd1);
-		char *line2 = get_next_line(fd2);
-		char *line3 = get_next_line(fd3);
-
-		if (line1)
-			printf("line1->%d: %s", i, line1);
-		else
-			printf("line1->%d: null\n", i);
-		if (line2)
-			printf("line2->%d: %s", i, line2);
-		else
-			printf("line2->%d: null\n", i);
-		if (line3)
-			printf("line3->%d: %s", i, line3);
-		else
-			printf("line3->%d: null\n", i);
-		free(line1);
-		free(line2);
-		free(line3);
-		i++;
-		if (!line1 && !line2 && !line3)//  
-			break;
-	}
-	close(fd1);
-	close(fd2);
-	close(fd3);
 }

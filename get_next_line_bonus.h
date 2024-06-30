4cb9fa40dca369265092f302acdf7606617175ac
iamdudumon <kksy4511@naver.com>
2024-06-30 12:46:32 +0900

    feat: new version bonus 파일 추가

---
+++ get_next_line_bonus.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 13:34:17 by dukim             #+#    #+#             */
+/*   Updated: 2024/06/28 13:34:18 by dukim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+typedef struct s_list
+{
+	int				fd;
+	char			*backup;
+	struct s_list	*next;
+	struct s_list	*prev;
+}					t_list;
+
+char	*get_next_line(int fd);
+
+char	*ft_get_chridx(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2, char end);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+t_list	*ft_lstnew(int fd);
+void	del_node(t_list **list, t_list *node);
+
+#endif
---
+++ get_next_line_bunus.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dukim <dukim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 13:34:12 by dukim             #+#    #+#             */
+/*   Updated: 2024/06/28 13:34:14 by dukim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static char	*read_buffer(t_list	*node)
+{
+	char	*buf;
+	char	*res;
+	ssize_t	size;
+
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	size = read(node->fd, buf, BUFFER_SIZE);
+	buf[size] = '\0';
+	res = ft_strjoin(node->backup, buf, '\0');
+	free(node->backup);
+	free(buf);
+	if (!ft_get_chridx(res, '\n'))
+	{
+		if (size == 0)
+		{
+			node->backup = 0;
+			return (res);
+		}
+		node->backup = res;
+		return (0);
+	}
+	buf = ft_strjoin("", res, '\n');
+	node->backup = ft_strjoin(ft_get_chridx(res, '\n') + 1, "", '\0');
+	free(res);
+	return (buf);
+}
+
+static char	*get_one_line(t_list *node)
+{
+	char	*buf;
+
+	buf = read_buffer(node);
+	while (!buf)
+		buf = read_buffer(node);
+	if (*buf == '\0' && !(node->backup))
+	{
+		free(buf);
+		return (0);
+	}
+	return (buf);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_list	*list;
+	t_list			*node;
+	char			*res;
+
+	node = list;
+	while (node)
+	{
+		if (node->fd == fd)
+			break ;
+		node = node->next;
+	}
+	if (!node)
+	{
+		node = ft_lstnew(fd);
+		ft_lstadd_back(&list, node);
+	}
+	res = get_one_line(node);
+	if (!res)
+		del_node(&list, node);
+	return (res);
+}

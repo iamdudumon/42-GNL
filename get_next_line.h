e04f16e7e44d2431ef730550e1cf1b86875cd39e
iamdudumon <kksy4511@naver.com>
2024-06-30 12:43:11 +0900

    new version-1

--- get_next_line.h
+++ get_next_line.h
@@ -13,4 +13,27 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+typedef struct s_list
+{
+	int				fd;
+	char			*backup;
+	struct s_list	*next;
+	struct s_list	*prev;
+}					t_list;
+
+char	*get_next_line(int fd);
+
+char	*ft_get_chridx(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2, char end);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+t_list	*ft_lstnew(int fd);
+void	del_node(t_list **list, t_list *node);
+
 #endif
